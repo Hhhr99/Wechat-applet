58479a05285774e114dc149a8a6a9810489beca7
Hhhr <Hhhr1214@gmail.com>
2021-06-02 15:41:53 +0800

    页面的生命周期

--- day04/pages/index/index.js
+++ day04/pages/index/index.js
@@ -4,54 +4,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-    tabItems: [{
-        key: "index",
-        name: "首页"
-      },
-      {
-        key: "news",
-        name: "新闻"
-      }, {
-        key: "about",
-        name: "关于"
-      }
-    ]
-  },
-  // b.2 定义一个 tabs点击处理函数
-  //b.3 到时 tabs 组件传来值 通过 msg.detail 就可以获取得到
-  handleTabsClick: function (msg) {
-    console.log(msg.detail);
-  },
-  handleTap: function (msg) {
-    console.log(msg.detail);
+
   },
 
   /**
    * 生命周期函数--监听页面加载
+   * 传统url: query?good_id=1000
+   * window.location.href
+   * 微信小程序页面 允许的
+   * /pages/index/index?good_id=1000
    */
   onLoad: function (options) {
-
+    // {good_id:1000} => options
+    console.log("options:", options);
+    console.log("index 页面的 onLoad执行了");
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
+   * 有点类似 jquery ready 函数
    */
   onReady: function () {
-
+    console.log("index 页面  初次渲染完成")
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log("index 页面看见了");
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log("index 页面隐藏了");
   },
 
   /**
@@ -65,14 +53,16 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log("执行 onPullDownRefresh");
+    // 马上停止下拉刷新效果
+    wx.stopPullDownRefresh();
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log("上拉触底事件");
   },
 
   /**
