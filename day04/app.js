1285b8e45cf62f931b255c0fb63b3391dc35047e
Hhhr <Hhhr1214@gmail.com>
2021-06-02 15:36:24 +0800

    应用的生命周期

--- day04/app.js
+++ day04/app.js
@@ -1,19 +1,36 @@
-// app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    console.log("执行应用生命周期 onLaunch 函数");
   },
-  globalData: {
-    userInfo: null
-  }
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    // throw new Error("自己跑出来的错误的信息");
+    console.log("应用生命周期 onShow");
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    console.log("执行应用生命周期 onHide");
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    console.log("异常信息：", msg);
+    // console.warn("异常信息：",msg);
+    // console.error("异常信息：",msg);
+  },
+  onPageNotFound: function (msg) {
+    console.log(msg);
+  },
 })
