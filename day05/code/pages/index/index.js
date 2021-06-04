9c91d1d81f83c668ddaf507bdca545e1e5c78349
Hhhr <Hhhr1214@gmail.com>
2021-06-04 14:42:24 +0800

    首页轮播图-结构部分

--- day05/code/pages/index/index.js
+++ day05/code/pages/index/index.js
@@ -4,12 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    swiperData: []
   },
   // 事件处理函数
   bindViewTap() {
@@ -17,32 +12,66 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+  onLoad: function (options) {
+    // b.1 在 onLoad生命周期中发请求获取数据
+    // b.2 success 回调函数 把它定义成箭头函数方式，目的为了能够正确获取当前 this 
+    wx.request({
+      url: 'https://106.55.254.112/api/public/v1/home/swiperdata',
       success: (res) => {
-        console.log(res)
+        console.log(res.data);
+        // b.3 通过 res.data.message 轮播图中的数据
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          swiperData: res.data.message
         })
       }
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
